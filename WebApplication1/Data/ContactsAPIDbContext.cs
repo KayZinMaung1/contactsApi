941311293802ff7b3b6ba85ef51b7ec0167f89fd
KayZinMaung1 <kayzinmg363@gmail.com>
2022-10-07 11:56:21 +0630

    Add project files.

---
+++ WebApplication1/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication1.Data
+{
+    public class ContactsAPIDbContext: DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
